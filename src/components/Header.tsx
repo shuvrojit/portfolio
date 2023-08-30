ca1ba343d5fb33f00ebaa12b86a81b07ea80ed5a
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-08-30 23:08:54 +0600

    Add header content

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import "../styles/Header.css"
+
+const Header = () => {
+  return (
+    <>
+      <header>
+      <div className="header-intro">
+        <p>Shuvrojit Biswas</p>
+        <p>Full Stack Web Developer</p>
+      </div>
+      <nav className="header-navigation">
+        <a href="#"><li>About</li></a>
+        <a href="#"><li>Works</li></a>
+        <a href="#"><li>Blog</li></a>
+      </nav>
+      <a href="#"><button>Contact</button></a>
+      </header>
+    </>
+  )
+}
+
+
+export default Header
